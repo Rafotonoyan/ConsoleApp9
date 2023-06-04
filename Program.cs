2366f602d220cbfa3764a352c5b1cfb58d8b51db
Admin110 <Admin110@ADMIN>
2023-06-04 20:19:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 70; i++);
+            {
+                if (int number1 % == 0; number1 %= 2);  
+            else if (number1 = number1 * 3 + 1);
+            while (number1 != 1) ;
+            if (number1 >= 50 && number1 <= 70) ; 
+
+
+             ete num 1 %==0 ; apa num 1 %= 2 ;
+               
+                hakarak depqum num1=number1*3+1
+                    gorcoxutyunn katari while i mech minchev num1!=1
+
+                    while i tox ani minchev num1>=50 ev num 1 <= 70;
+
+        }
+         
+    }
+}
